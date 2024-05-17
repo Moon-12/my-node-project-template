ec731d4a0fe5a26f2d5179d09e527ae0be33aaf4
Moon-12 <ashwijanayak@gmail.com>
2024-05-17 11:20:30 -0400

    error handling added + morgan & nodemon installed

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 const dummyEndpointRoutes = require("./api/routes/dummyEndpointRoutes");
 
@@ -9,6 +11,22 @@ const dummyEndpointRoutes = require("./api/routes/dummyEndpointRoutes");
 //   });
 // });
 
+app.use(morgan("dev"));
 app.use("/dummy-endpoint", dummyEndpointRoutes);
 
+//handling error
+
+app.use((req, res, next) => {
+  const error = new Error("not found");
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 module.exports = app;
